2b9a4a2dc53ad8e23bd102a180d793d9b9f52532
Ufuk Baran Güler <91633812+Ufuk-Guler@users.noreply.github.com>
2025-12-23 00:02:24 +0300

    backend initialization 2

---
+++ backend/src/test/java/com/driveme/backend/DrivemeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driveme.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivemeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
